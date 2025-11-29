b8ad2983dee695fad1d706b3072bfb613e815ae0
253885899 <104180119+253885899@users.noreply.github.com>
2025-11-29 22:26:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const nextConfig = {
-  output: 'standalone',
-  eslint: {
-    dirs: ['src'],
+  //output: 'standalone',
+  //eslint: {
+    //dirs: ['src'],
     webpack: (config, { isServer }) => {
         if (isServer) {
             // 避免 webpack 尝试打包 async_hooks，因为它由运行时环境提供
